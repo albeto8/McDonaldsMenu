f4aadfcee626c5984475b07c43894e6e51cbac98
Mario Alberto Barragán Espinosa <mariosdev05@gmail.com>
2022-07-20 18:41:16 -0500

    Set CollectionViewTableViewCell for MainMenuViewController

--- McDonaldsMenu/Controllers/MainMenuViewController.swift
+++ McDonaldsMenu/Controllers/MainMenuViewController.swift
@@ -7,10 +7,67 @@
 
 import UIKit
 
+struct MenuCategory {
+    let title: String
+    let products: [Product]
+}
+
+struct Product {
+    let name: String
+}
+
 final class MainMenuViewController: UIViewController {
+    
+    private let tableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .grouped)
+        tableView.register(CollectionViewTableViewCell.self, forCellReuseIdentifier: CollectionViewTableViewCell.identifier)
+        return tableView
+    }()
+    
+    var menuCategories = [MenuCategory]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        view.addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.frame = view.bounds
+    }
+    
+    func display(_ menuCategories: [MenuCategory]) {
+        self.menuCategories = menuCategories
+        self.tableView.reloadData()
+    }
+}
+
+extension MainMenuViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        menuCategories.count
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: CollectionViewTableViewCell.identifier, for: indexPath) as? CollectionViewTableViewCell else {
+            return UITableViewCell()
+        }
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        200
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        40
     }
 }
--- McDonaldsMenuTests/McDonaldsMenuTests.swift
+++ McDonaldsMenuTests/McDonaldsMenuTests.swift
@@ -12,6 +12,24 @@ class McDonaldsMenuTests: XCTestCase {
     func test_emptyScreen() {
         let sut = MainMenuViewController()
         
+        sut.loadViewIfNeeded()
+        
         assert(sut, device: .iPhoneX)
+    }
+    
+    func test_listWithItems() {
+        let sut = MainMenuViewController()
+        let beberageCategory = makeMenuCategory(title: "Beverages")
+        
+        sut.display([beberageCategory])
+        
+        sut.loadViewIfNeeded()
+        
+        record(sut, device: .iPhoneX)
+    }
+    
+    private func makeMenuCategory(title: String) -> MenuCategory {
+        let product = Product(name: "Coca-Cola")
+        return MenuCategory(title: title, products: [product])
     }
 }
