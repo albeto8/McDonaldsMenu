10b68a6dfc0c4f3fe76ee38e43acd307fe76d6d0
Mario Alberto Barragán Espinosa <mariosdev05@gmail.com>
2022-07-20 18:01:24 -0500

    Initial Commit

---
+++ McDonaldsMenu/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  McDonaldsMenu
+//
+//  Created by Mario Alberto Barragán Espinosa on 20/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
