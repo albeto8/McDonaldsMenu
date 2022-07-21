e8163089ed9eabac1bd90f9b9e10a39e39e6884e
Mario Alberto Barragán Espinosa <mariosdev05@gmail.com>
2022-07-20 21:23:12 -0500

    Change collection view cell size

--- McDonaldsMenu/Views/Cells/CollectionViewTableViewCell.swift
+++ McDonaldsMenu/Views/Cells/CollectionViewTableViewCell.swift
@@ -12,7 +12,7 @@ final class CollectionViewTableViewCell: UITableViewCell {
     
     private let collectionView: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
-        layout.itemSize = CGSize(width: 140, height: 200)
+        layout.itemSize = CGSize(width: 160, height: 160)
         layout.scrollDirection = .horizontal
         let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
         collectionView.register(ProductCollectionViewCell.self, forCellWithReuseIdentifier: ProductCollectionViewCell.identifier)
