447dc37723c817786c3ade0b51362839870f99bd
Mario Alberto Barragán Espinosa <mariosdev05@gmail.com>
2022-07-21 21:23:25 -0500

    Add new ui elments for detail view controller

--- McDonaldsMenu/Controllers/DetailViewController.swift
+++ McDonaldsMenu/Controllers/DetailViewController.swift
@@ -48,6 +48,16 @@ final class DetailViewController: UIViewController {
         return label
     }()
     
+    private let descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont(name: "Inter-Regular", size: 16)
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        label.textColor = .black
+        label.text = "The McDonald's Bacon, Egg & Cheese Biscuit breakfast sandwich features a warm, buttermilk biscuit brushed with real butter, thick cut Applewood smoked bacon, a fluffy folded egg, and a slice of melty American cheese. There are 460 calories in a Bacon, Egg & Cheese Biscuit at McDonald's. Try one today with a Premium Roast Coffee and order with Mobile Order & Pay on the McDonald's App!"
+        return label
+    }()
+    
     init(product: Product) {
         self.product = product
         super.init(nibName: nil, bundle: Bundle(for: type(of: self)))
@@ -64,6 +74,7 @@ final class DetailViewController: UIViewController {
         view.addSubview(productNameLabel)
         view.addSubview(priceContainerView)
         priceContainerView.addSubview(priceLabel)
+        view.addSubview(descriptionLabel)
         
         productImageView.centerX(inView: view)
         productImageView.anchor(top: view.topAnchor, paddingTop: 51)
@@ -75,6 +86,8 @@ final class DetailViewController: UIViewController {
         priceContainerView.centerX(inView: view)
         priceContainerView.anchor(top: productNameLabel.bottomAnchor, paddingTop: 28)
         priceLabel.center(inView: priceContainerView)
+        
+        descriptionLabel.anchor(top: priceContainerView.bottomAnchor, leading: view.leadingAnchor, trailing: view.trailingAnchor, paddingTop: 21, paddingLeading: 30, paddingTrailing: 30)
         
         configure(with: product)
     }
